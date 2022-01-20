a791bdcf64e9174af86d5ae04683e27077ed07f2
segfaultDelirium <82722872+segfaultDelirium@users.noreply.github.com>
2022-01-20 08:40:45 +0100

    add sass file watcher instead of webpack

--- static/src/js/index.js
+++
@@ -1,7 +0,0 @@
-import 'bootstrap';
-import '../sass/index.scss';
-import '../sass/header.scss';
-import '../sass/navbar.scss';
-import '../sass/login.scss';
-import '../sass/books.scss';
-console.log('hello world');
